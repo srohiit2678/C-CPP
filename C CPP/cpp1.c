45d27f6ccf018abe88345e67a168c35ed498a920
Rohit Sikhwal <rohitsikhwal99@gmail.com>
2023-12-09 23:17:46 +0530

    try
    try to solve a hacker rank question

--- C CPP/cpp1.c
+++ C CPP/cpp1.c
@@ -1,8 +1,54 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main()
-{
- cout<<"i am rohit";
- return 0;
+//Complete the following function.
+
+int marks_summation(int* marks, int number_of_students, char gender) {
+  //Write your code here.
+  int sum=0;
+  switch (gender) {
+  case 'b':{
+      for(int i=0;i<number_of_students;i++){
+          sum = sum + marks[i];
+            i++;
+      }
+      break;
+  }      
+  case 'g': {
+      for(int i=1;i<number_of_students;i++){
+        if(i%2==0){
+          continue;
+          }
+        else
+        {
+            sum = sum + marks[i];
+        }
+          break;
+
+    }
+    default:
+    break;
+    }
+return sum;
+}
+}
+int main(){
+    int number_of_students;
+    char gender;
+    int sum;
+    printf("enter no.");
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+    return 0;
 }
