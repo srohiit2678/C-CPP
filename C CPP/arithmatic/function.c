20b87172bc92b5137793415e6a5eca374278ceea
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-02 04:13:09 +0530

    Update function.c

--- C CPP/arithmatic/function.c
+++ C CPP/arithmatic/function.c
@@ -1,12 +1,26 @@
-#include<stdio.h> //
-void show(); //proto type or function decleration.
-void main()
-{
-    printf("i am in function.\n");
-    show(); //function calling.
-    printf("I AM BACK IN MAIN FUNCTION.\n");
-}
-void show()  //define the function
-{
-    printf("i am in show function.\n");
-}
+
+
+
+
+
+
+
+// #include<stdio.h>
+
+//  //
+// void show(); //proto type or function decleration.
+// int main(){  
+//     char ch[100];
+//     scanf("%c",ch);
+//   for(int i=0;i<strlen(ch);i++){
+//     printf("%s",ch);     
+    
+//  }
+
+// return 0;
+// }
+
+// void show()  //define the function
+// {
+//     printf("i am in show function.\n");
+// }
