52d2441be96b1c432fd523300c76e203d2c601e3
srohiit2678 <rohitsikhwal99@gmail.com>
2024-10-02 04:13:03 +0530

    Update cpp1.cpp

--- C CPP/arithmatic/cpp1.cpp
+++ C CPP/arithmatic/cpp1.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 using namespace std;
+class demo{
+    public :
+    int x,y;
+    void get(int x,int y)
+    {
+       this-> x=x;
+        this-> y=y;
+    }
+    void show(){
+    cout<<x<<endl;
+    cout<<y<<endl;
+    } 
+};
 
-int main()
-{
- cout<<"i am rohit";
- return 0;
+int main(){
+ demo d;
+   d.show();
+   d.get(10,20);
+    d.show();
+return 0;
 }
